bd89415046bfe295d9fe3ba7771658bcb66595e4
AsunaYuuki197 <tamhau197@gmail.com>
2024-03-29 22:35:29 +0700

    Update UML + add views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class Staff(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    Phone = models.IntegerField()
+    Address = models.TextField() 
+    Avatar = models.FileField()
+    Role = models.CharField()
+    Certificate = models.CharField()
+    Specialize = models.CharField()
+    Availablle = models.BooleanField()
