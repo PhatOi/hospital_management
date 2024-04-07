da39b0e1b0e8589753f36d7f95a2a64339197529
Le Thuan Phat <163813842+PhatOi@users.noreply.github.com>
2024-04-07 18:45:18 +0700

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -18,4 +18,15 @@ urlpatterns = [
     path('healthcareStaff/storage', views.healthcareStaff__Storage, name='healthcareStaff__Storage'),
     # HEALTHCARE STAFF
 
+    # PATIENT
+    path('Patient/Medical_History', views.Patient__Medical_History, name='Patient__Medical_History'),
+    path('Patient/mainpage', views.Patient__MainPage, name='Patient__MainPage'),
+    path('Patient/patient_info', views.Patient__Patient_Info, name='Patient__Patient_Info'),
+    path('Patient/re-examination', views.Patient__Re_examination, name='Patient__Re_examination'),
+    path('Patient/update_info', views.Patient__Update_Info, name='Patient__Update_Info'),
+    path('Patient/login', views.Patient__Login, name='Patient__Login'),
+    path('Patient/register', views.Patient__Register, name='Patient__Register'),
+    path('Patient/forgot_password', views.Patient__Forgot_password, name='Patient__Forgot_password'),
+    path('Patient/reset_password', views.Patient__Reset_password, name='Patient__Reset_password'),
+    # PATIENT
 ]
